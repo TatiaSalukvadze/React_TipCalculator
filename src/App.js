f850fc40a5ef614fa22dc2199cd606d2d484bfcd
TatiaSalukvadze <tatiasaluqvadze86@gmail.com>
2023-10-14 13:25:00 +0400

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import logo from "./images/logo.svg";
+import person from "./images/icon-person.svg";
 function App() {
   const [tanxa, setTanxa] = useState(0);
   const [tip, setTip] = useState(0);
@@ -10,49 +11,91 @@ function App() {
 
   const handleChange = (event) => setTanxa(event.target.value);
   const handleChange1 = (event) => setPep(event.target.value);
-  const calc = () => {
-    setNewt((Number(tanxa) * Number(tip)) / 100 / pep);
-    setSul((Number(tanxa) / pep + newt).toFixed(2));
-  };
+
+  useEffect(() => {
+    if (Number(tanxa) > 0 && Number(tip) > 0 && Number(pep) > 0) {
+      setNewt((Number(tanxa) * Number(tip)) / 100 / pep);
+      setSul((Number(tanxa) / pep + newt).toFixed(2));
+    }
+  }, [tanxa, tip, pep]);
+
   return (
-    <div className="App">
-      <br />
-      <form>
-        <label for="inp">თანხა? </label>
-        <input type="text" name="inp" placeholder="$" onChange={handleChange} />
-        <br />
-        <br />
-        <div>
-          <Tip val={5} setT={setTip} />
-          <Tip val={10} setT={setTip} />
-          <Tip val={15} setT={setTip} />
-          <Tip val={25} setT={setTip} />
-          <Tip val={50} setT={setTip} />
-        </div>
-        <p>ადამიანთა რაოდენობა?</p>
+    <div className="app">
+      <img src={logo} alt="logo" className="logo"></img>
+      <div className="wrapper">
+        <form>
+          <div className="first">
+            <label htmlFor="inp">Bill </label>
 
-        <input
-          type="number"
-          name="npep"
-          placeholder="1"
-          onChange={handleChange1}
-        />
+            <input
+              type="text"
+              name="inp"
+              placeholder="$"
+              onChange={handleChange}
+              className="bill"
+            />
 
-        <button name="data" type="button" onClick={() => calc()}>
-          Submit
-        </button>
+            <label>Select Tip %</label>
 
-        <p>
-          {newt} sul: {sul}
-        </p>
-        <input type="reset" />
-      </form>
+            <div className="grid">
+              <Tip val={5} setT={setTip} />
+              <Tip val={10} setT={setTip} />
+              <Tip val={15} setT={setTip} />
+              <Tip val={25} setT={setTip} />
+              <Tip val={50} setT={setTip} />
+            </div>
+            <label htmlFor="npep">Number of People</label>
+
+            <input
+              type="number"
+              name="npep"
+              placeholder="1"
+              onChange={handleChange1}
+              className="people"
+            />
+
+            {/* <button name="data" type="button" onClick={() => calc()}>
+              Submit
+            </button> */}
+          </div>
+          <div className="second">
+            <div className="box">
+              <div>
+                <h3>Tip Amount</h3>
+                <span>/ person</span>
+              </div>
+              <p className="result">${newt}</p>
+            </div>
+            <div className="box">
+              <div>
+                <h3>Total</h3>
+                <span>/ person</span>
+              </div>
+              <p className="result">${sul}</p>
+            </div>
+            <button
+              type="reset"
+              className="reset"
+              onClick={() => {
+                setSul(0);
+                setNewt(0);
+              }}
+            >
+              Reset
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
 
 function Tip({ val, setT }) {
-  return <input type="button" onClick={() => setT(val)} value={val} />;
+  return (
+    <button type="button" onClick={() => setT(val)} className="tip">
+      {val}%
+    </button>
+  );
 }
 
 export default App;
